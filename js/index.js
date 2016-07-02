2e2fd11af401ef760531f82af7dc210ef93c3549
CMYKpixels <julien.alsina@gmail.com>
2016-07-02 14:13:07 +0200

    Revert "Improving app"
    This reverts commit e971b20b8a8bc400b4bfab7b9616af05d755135c.
    
    # Conflicts:
    #	views/index.ejs

--- js/index.js
+++ js/index.js
@@ -1,44 +1,21 @@
-// var socket = io.connect('http://192.168.10.120:3000');//pierre IP 103 //thomas IP 144
+var socket = io.connect('http://localhost:3000');
+
+
 //Le serveur détecte la connexion
 //socket.on('logged', function(userID){
-//$('body').append('<h1> Welcome user '+userID+'</h1>');
+//	$('body').append('<h1> Welcome user '+userID+'</h1>');
 //});
-var socket = io.connect('http://localhost:3000');
-$("#formulaire").submit( function (event){
-// prevent reload page
-    event.preventDefault ();
-    $(this).addClass ("hidden");
 
-// affiche la liste des utilisateurs
-    $("#connected_users").removeClass ("hidden");
 
-// contact server
+$("formulaire").submit(function(event) {
+    event.preventDefault();
     var user = {
         username : $("#username").val(),
         email : $("#mail").val()
+        
     };
-    socket.emit ("logged", user);
-    socket.on ('gestion_user', function(all_users){
-		console.log(all_users);
-		var $ul = $("#user_list");
-		$ul.html ('');
-    	for (var i = 0; i < all_users.length; i++) {
-    		// for (var i in all_users) {
-				$ul.append('<li>['+all_users[i].username+']</li>');	
-	    	}
-    	});
-    });
-
-
-$("#log").submit( function (event) {
-    event.preventDefault();
-    var message = $("#msg").val();
-    socket.emit('send_msg', message);
-    $('#msg').val('');
 });
 
-socket.on('all_msg', function (socketUser, message) {
-    console.log(message);
-    //console.log(msg);
-    $("#list_message").append('<panel>['+socketUser.username+' :][ '+message+']</panel><br>');
-});
+//L'utilisateur indique qu'il est connecté
+//!=socket.on où le serveur surveille les connexions
+socket.emit('logged', user);
